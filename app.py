b13c5b3622ec8b0bec056439ef1d0572bdd0e4e5
Pranali <yourgmail@gmail.com>
2026-06-29 16:49:33 +0530

    fix flask app structure for render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 from predict import predict_intent
 from chatbot import get_response
@@ -6,23 +7,17 @@ from save_chat import save_chat
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-
     return render_template("index.html")
-
 
 
 @app.route("/chat", methods=["POST"])
 def chat():
-
     data = request.get_json()
-
     user_message = data["message"]
 
     intent = predict_intent(user_message)
-
     response = get_response(intent)
 
     save_chat(user_message, response, intent)
@@ -31,11 +26,6 @@ def chat():
         "response": response
     })
 
-
-
-if __name__ == "__main__":
-
-   import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
